a7236c304965c5437e689fb890572cbe1b8523e5
Bryce Kahle <bkahle@gmail.com>
2017-07-19 16:55:47 -0400

    Create netlifyctl.rb

---
+++ netlifyctl.rb
@@ -0,0 +1,15 @@
+class NetlifyCTL < Formula
+  desc "CLI to interact with netlify.com"
+  homepage "https://github.com/netlify/netlifyctl"
+  url "https://github.com/netlify/netlifyctl/releases/download/v0.1.4/netlifyctl-darwin-amd64.tar.gz"
+  version "0.1.4"
+  sha256 "c970773c7448e7d0a87c08ac5908c94adb554eadb88ba89faf9eee6dc14f296b"
+  
+  def install
+    bin.install "netlifyctl"
+  end
+
+  test do
+    system "#{bin}/netlifyctl", "version"
+  end
+end
