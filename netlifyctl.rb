98ba3f68c8476ff07528ada49b6278527fc04bd8
David Calavera <david.calavera@gmail.com>
2018-03-01 14:50:34 -0800

    Update to v0.3.2

--- netlifyctl.rb
+++ netlifyctl.rb
@@ -1,9 +1,9 @@
 class Netlifyctl < Formula
   desc "CLI to interact with netlify.com"
   homepage "https://github.com/netlify/netlifyctl"
-  url "https://github.com/netlify/netlifyctl/releases/download/v0.3.1/netlifyctl-darwin-amd64-0.3.1.tar.gz"
-  version "0.3.1"
-  sha256 "e0e0b77b44683bec87227e086d8abbff430cb0990a65666bccb0c65fec2dc9e6"
+  url "https://github.com/netlify/netlifyctl/releases/download/v0.3.2/netlifyctl-darwin-amd64-0.3.2.tar.gz"
+  version "0.3.2"
+  sha256 "16d756046392873b80d4ec806e5b8bc66a3fe000d6304f6bb7997114e4bb2712"
   
   def install
     bin.install "netlifyctl"
