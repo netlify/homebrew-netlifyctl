d4e650cc57e819b256d925fc797325da1f96160d
David Calavera <david.calavera@gmail.com>
2018-06-07 08:12:25 -0700

    Update formula to version 0.4.0.
    Signed-off-by: David Calavera <david.calavera@gmail.com>

--- netlifyctl.rb
+++ netlifyctl.rb
@@ -1,9 +1,9 @@
 class Netlifyctl < Formula
   desc "CLI to interact with netlify.com"
   homepage "https://github.com/netlify/netlifyctl"
-  url "https://github.com/netlify/netlifyctl/releases/download/v0.3.4/netlifyctl-darwin-amd64-0.3.4.tar.gz"
-  version "0.3.4"
-  sha256 "e7368b75e1395c1a7d1797690aa094015de6e4d96c2d2256269d14eef747b1d7"
+  url "https://github.com/netlify/netlifyctl/releases/download/v0.4.0/netlifyctl-darwin-amd64-0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "91e1ca178167d89771fe8864728d736f2f3ce87c4044afd0702367c2d803e834"
 
   def install
     bin.install "netlifyctl"
